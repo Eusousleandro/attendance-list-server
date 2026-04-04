b2141351a416176e9161036f5206e9fb2b91696e
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-04 17:42:03 -0300

    Modified config database.

--- src/config/database/connection.ts
+++ src/config/database/connection.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import mysql from 'mysql2'
 
 export const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST, 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
