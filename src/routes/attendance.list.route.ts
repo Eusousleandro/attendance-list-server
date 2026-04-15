63e471e4557d557a5d862863fe2ac4903728afc1
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-14 21:54:33 -0300

    Modified in route attendance

--- src/routes/attendance.list.route.ts
+++ src/routes/attendance.list.route.ts
@@ -10,7 +10,7 @@ const attendanceListService = new AttendanceListService(attendanceListRepository
 const attendanceListController = new AttendanceListController(attendanceListService)
 
 router.get('/attendance', attendanceListController.getAll.bind(attendanceListController))
-router.get('/attendace/:id', attendanceListController.getById.bind(attendanceListController))
+router.get('/attendance/:id', attendanceListController.getById.bind(attendanceListController))
 router.post('/create/attendance', attendanceListController.createList.bind(attendanceListController))
 router.put('/update/attendance', attendanceListController.updateList.bind(attendanceListController))
 router.delete('/delete/attendance', attendanceListController.deleteList.bind(attendanceListController))
